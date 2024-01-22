744d4757da91c481fd2f2b80e997bcc282ec1fb2
Kevin C. Krinke <kevin@krinke.ca>
2024-01-22 10:04:37 -0500

    CoreLibs.mk: updated to v0.1.13

--- CoreLibs.mk
+++ CoreLibs.mk
@@ -4,7 +4,7 @@ SHELL := /bin/bash
 
 VERSION_TAGS        += CORELIBS
 CORELIBS_MK_SUMMARY := Go-CoreLibs.mk
-CORELIBS_MK_VERSION := v0.1.11
+CORELIBS_MK_VERSION := v0.1.13
 
 GOPKG_KEYS          ?=
 GOPKG_AUTO_CORELIBS ?= true
@@ -12,6 +12,7 @@ LOCAL_CORELIBS_PATH ?= ..
 
 .PHONY: help version
 .PHONY: local unlocal be-update tidy
+.PHONE: corelibs packages
 .PHONY: deps build clean fmt
 .PHONY: test coverage goconvey reportcard
 
@@ -34,9 +35,10 @@ endef
 
 define __list_corelibs
 $(shell grep -h -v '^module' go.mod \
-		| grep 'go-corelibs/' \
-		| grep -v "${CORELIB_PKG}" \
-		| awk '{print $$1}' \
+		| grep -P '^(require)?\s*github.com/go-corelibs/' \
+		| grep -v "github.com/${CORELIB_PKG} v" \
+		| grep -v "// indirect" \
+		| perl -pe 's!^(require)?\s*!!;s!\s+v\d+(.\d)*.*$$!!;' \
 		| sort -u -V \
 		| while read MODULE; do \
 			NAME=$$(basename "$${MODULE}"); \
@@ -59,6 +61,7 @@ help: export FOUND_LIBS=$(call __list_corelibs)
 help:
 	@echo "# usage: make <help|version>"
 	@echo "#        make <local|unlocal|be-update|tidy>"
+	@echo "#        make <corelibs|packages>"
 	@echo "#        make <deps|build|clean|fmt>"
 	@echo "#        make <test|coverage|goconvey|reportcard>"
 	@echo "#"
@@ -71,6 +74,9 @@ help:
 	@echo "#  unlocal        - go mod edit -dropreplace"
 	@echo "#  be-update      - go get @latest"
 	@echo "#  tidy           - go mod tidy"
+	@echo "#"
+	@echo "#  corelibs       - list detected go-corelibs"
+	@echo "#  packages       - list configured GOPKGS"
 	@echo "#"
 	@echo "#  deps           - install dependencies"
 	@echo "#  build          - go build -v ./..."
@@ -98,6 +104,26 @@ help:
 				echo "#  - $${pkg}"; \
 			done; \
 		fi; \
+	fi
+
+corelibs: export FOUND_LIBS=$(call __list_corelibs)
+corelibs:
+	@if [ -n "$${FOUND_LIBS}" ]; then \
+		for FOUND in $${FOUND_LIBS}; do \
+			echo "# $${FOUND}"; \
+		done; \
+	else \
+		echo "# no go-corelibs detected"; \
+	fi
+
+packages: export FOUND_PKGS=$(call __list_gopkgs)
+packages:
+	@if [ -n "$${FOUND_PKGS}" ]; then \
+		for FOUND in $${FOUND_PKGS}; do \
+			echo "# $${FOUND}"; \
+		done; \
+	else \
+		echo "# no GOPKGS configured"; \
 	fi
 
 version: LIST=$(foreach key,${VERSION_TAGS},\\n# $($(key)_MK_SUMMARY) $($(key)_MK_VERSION))
@@ -197,7 +223,7 @@ reportcard:
 	@echo "#: go vet"
 	@go vet ./...
 	@echo "#: gocyclo"
-	@gocyclo -over 15 `find * -name "*.go"`
+	@gocyclo -over 15 `find * -name "*.go"` || true
 	@echo "#: ineffassign"
 	@ineffassign ./...
 	@echo "#: misspell"
